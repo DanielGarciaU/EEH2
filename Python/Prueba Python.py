ebc2ade8b64bab046342f1b9c40610d86863f3ed
DanielGarciaU <121569512+DanielGarciaU@users.noreply.github.com>
2022-12-28 15:49:52 -0600

    Nueva linea 2

---
+++ Python/Prueba Python.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pyodbc 
+
+conn_str = ("Driver={SQL Server};"
+            "Server=ASVRDB03;"
+            "Database=CONTROLENERGIA;"
+            "UID=daniel.garcia;"
+            "PWD=Daniel2022*;")
+conn = pyodbc.connect(conn_str)
+
+df = pd.read_sql('SELECT top 100* FROM historico_consumos', conn)
+
+print(df)
+print(type(df))
